f3a369a9250b758d4dd177f593ccfa2bc7c95a02
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-04-01 23:15:07 -0500

    feat: Styles were added to the home page and navbar animation was added.

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
@@ -8,6 +8,18 @@ import { NavbarComponent } from '../../shared/navbar/navbar.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    // Toggle menu functionality
+    const toggle = document.querySelector('.toggle');
+    const container = document.querySelector('.container');
+    const nav = document.querySelector('.nav');
+
+    toggle?.addEventListener('click', () => {
+      container?.classList.toggle('active');
+      nav?.classList.toggle('active');
+    });
+  }
 }
