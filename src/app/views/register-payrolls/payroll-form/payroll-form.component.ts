f1f7cc3069797b92d8ff943d4c7997da18184007
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-18 21:34:02 -0500

    feat: registerPayrollComponent was ended

--- src/app/views/register-payrolls/payroll-form/payroll-form.component.ts
+++ src/app/views/register-payrolls/payroll-form/payroll-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-payroll-form',
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class PayrollFormComponent {
 
+  @Output() close = new EventEmitter<void>();
+
+  closeForm() {
+    this.close.emit();
+  }
+  
 }
