e28ff92bc6c99606e723ea5854a402eeb308d6cf
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-18 20:32:45 -0500

    feat: EditSchedulesComponent was ended

--- src/app/views/edit-schedules/edit-schedules.component.ts
+++ src/app/views/edit-schedules/edit-schedules.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from '../../shared/topbar/topbar.component';
+import { EditFormComponent } from './edit-form/edit-form.component';
+
 
 @Component({
   selector: 'app-edit-schedules',
   standalone: true,
-  imports: [],
+  imports: [TopbarComponent, EditFormComponent],
   templateUrl: './edit-schedules.component.html',
   styleUrl: './edit-schedules.component.css'
 })
+
 export class EditSchedulesComponent {
+
+  ngOnInit(): void {
+    // Toggle menu functionality
+    const toggle = document.querySelector('.toggle');
+    const container = document.querySelector('.container');
+    const nav = document.querySelector('.nav');
+
+    toggle?.addEventListener('click', () => {
+      container?.classList.toggle('active');
+      nav?.classList.toggle('active');
+    });
+  }
 
 }
