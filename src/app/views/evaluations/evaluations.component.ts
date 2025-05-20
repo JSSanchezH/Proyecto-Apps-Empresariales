d8a5cf00c1979826fe549bb6e0c42f895f4fc52a
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-19 23:40:43 -0500

    feat: Evaluation Component was ended

--- src/app/views/evaluations/evaluations.component.ts
+++ src/app/views/evaluations/evaluations.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from '../../shared/topbar/topbar.component';
+import { EvalautionFormComponent } from './evalaution-form/evalaution-form.component';
 
 @Component({
   selector: 'app-evaluations',
   standalone: true,
-  imports: [],
+  imports: [TopbarComponent, EvalautionFormComponent],
   templateUrl: './evaluations.component.html',
   styleUrl: './evaluations.component.css'
 })
 export class EvaluationsComponent {
+
+  ngOnInit(): void {
+    // Toggle menu functionality
+    const toggle = document.querySelector('.toggle');
+    const container = document.querySelector('.container');
+    const nav = document.querySelector('.nav');
+
+    toggle?.addEventListener('click', () => {
+      container?.classList.toggle('active');
+      nav?.classList.toggle('active');
+    });
+  }
 
 }
