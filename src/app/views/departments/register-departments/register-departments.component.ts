2f902e45a5393df025be488d240a505968293958
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-20 01:44:58 -0500

    feat: Deparment component was ended

--- src/app/views/departments/register-departments/register-departments.component.ts
+++ src/app/views/departments/register-departments/register-departments.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-register-departments',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './register-departments.component.html',
   styleUrl: './register-departments.component.css'
 })
