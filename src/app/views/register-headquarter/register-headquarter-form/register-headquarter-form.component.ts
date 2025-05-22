74bee4b336121f7fb7a551929d9e4eba6e477b9b
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-21 23:24:52 -0500

    feat: registerHeadquarter was updated

--- src/app/views/register-headquarter/register-headquarter-form/register-headquarter-form.component.ts
+++ src/app/views/register-headquarter/register-headquarter-form/register-headquarter-form.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputFieldComponent } from '../../../shared/input-field/input-field.component';
 
 @Component({
   selector: 'app-register-headquarter-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InputFieldComponent],
   templateUrl: './register-headquarter-form.component.html',
   styleUrl: './register-headquarter-form.component.css'
 })
 export class RegisterHeadquarterFormComponent {
+
+  form = {
+    name: '',
+    address: '',
+    phone: '',
+    city: '',
+    state: '',
+    country: '',
+    continent: ''
+  };
+
+  fields = [
+    { label: 'Name', icon: 'bx bx-user', type: 'text', placeholder: 'Full Name' },
+    { label: 'Address', icon: 'bx bx-map', type: 'text', placeholder: 'Address' },
+    { label: 'Phone', icon: 'bx bx-phone', type: 'tel', placeholder: 'Phone Number' },
+    {
+      label: 'City',
+      icon: 'bx bx-buildings',
+      type: 'select',
+      placeholder: 'Select City',
+      options: ['New York', 'Los Angeles', 'London']
+    },
+    {
+      label: 'State',
+      icon: 'bx bx-map-pin',
+      type: 'select',
+      placeholder: 'Select State',
+      options: ['California', 'Texas', 'Florida']
+    },
+    {
+      label: 'Country',
+      icon: 'bx bx-flag',
+      type: 'select',
+      placeholder: 'Select Country',
+      options: ['USA', 'UK', 'Canada']
+    },
+    {
+      label: 'Continent',
+      icon: 'bx bx-globe',
+      type: 'select',
+      placeholder: 'Select Continent',
+      options: ['North America', 'Europe', 'Asia']
+    }
+  ];
+
+  onSubmit() {
+    console.log('Headquarter info registered:', this.form);
+  }
 
 }
