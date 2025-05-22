c5b59ecbe5bb736cf44a742d311b784ecbe470e5
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-21 23:32:44 -0500

    feat: edit deparmets form was added on edit deparment

---
+++ src/app/views/edit-deparment/edit-deparment-form/edit-deparment-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-deparment-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-deparment-form.component.html',
+  styleUrl: './edit-deparment-form.component.css'
+})
+export class EditDeparmentFormComponent {
+
+}
