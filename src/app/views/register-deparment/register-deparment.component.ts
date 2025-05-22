8542a6e4ef53ab0681289bb12eadc9ebc12b6e43
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-21 23:57:12 -0500

    feat: registerDepartment was ended

--- src/app/views/register-deparment/register-deparment.component.ts
+++ src/app/views/register-deparment/register-deparment.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from '../../shared/topbar/topbar.component';
+import { RegisterDeparmentFormComponent } from './register-deparment-form/register-deparment-form.component';
 
 @Component({
   selector: 'app-register-deparment',
   standalone: true,
-  imports: [],
+  imports: [TopbarComponent, RegisterDeparmentFormComponent],
   templateUrl: './register-deparment.component.html',
   styleUrl: './register-deparment.component.css'
 })
 export class RegisterDeparmentComponent {
+
+  ngOnInit(): void {
+    // Toggle menu functionality
+    const toggle = document.querySelector('.toggle');
+    const container = document.querySelector('.container');
+    const nav = document.querySelector('.nav');
+
+    toggle?.addEventListener('click', () => {
+      container?.classList.toggle('active');
+      nav?.classList.toggle('active');
+    });
+  }
 
 }
