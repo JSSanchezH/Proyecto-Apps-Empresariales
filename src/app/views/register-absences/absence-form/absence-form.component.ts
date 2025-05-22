3ddcd59ae5fb66ac6bac5025b1a247c354aa8c46
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-21 23:18:17 -0500

    feat: registerAbsencesComponent was updated

--- src/app/views/register-absences/absence-form/absence-form.component.ts
+++ src/app/views/register-absences/absence-form/absence-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputFieldComponent } from '../../../shared/input-field/input-field.component';
 
 @Component({
   selector: 'app-absence-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InputFieldComponent],
   templateUrl: './absence-form.component.html',
   styleUrl: './absence-form.component.css'
 })
@@ -11,8 +13,32 @@ export class AbsenceFormComponent {
 
   @Output() close = new EventEmitter<void>();
 
+  form = {
+    absenceType: '',
+    startDate: '',
+    endDate: '',
+    description: ''
+  };
+
+  fields = [
+    {
+      label: 'Absence Type',
+      icon: 'bx bx-credit-card',
+      type: 'select',
+      placeholder: 'Select Absence Type',
+      options: ['Sick Leave', 'Vacation', 'Personal', 'Unpaid Leave']
+    },
+    { label: 'Start Date', icon: 'bx bx-calendar', type: 'date', placeholder: 'Start Date' },
+    { label: 'End Date', icon: 'bx bx-calendar', type: 'date', placeholder: 'End Date' },
+    { label: 'Description', icon: 'bx bx-note', type: 'textarea', placeholder: 'Enter a description...' }
+  ];
+
   closeForm() {
     this.close.emit();
+  }
+
+  onSubmit() {
+    console.log('Absence info registered:', this.form);
   }
 
 }
