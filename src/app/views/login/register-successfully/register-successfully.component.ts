eb690c2ffb1e08836425526386c398e70760ad1f
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-05-27 00:57:35 -0500

    feat: window register-successfully was ended

---
+++ src/app/views/login/register-successfully/register-successfully.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+
+@Component({
+  selector: 'app-register-successfully',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './register-successfully.component.html',
+  styleUrl: './register-successfully.component.css'
+})
+export class RegisterSuccessfullyComponent {
+
+}
