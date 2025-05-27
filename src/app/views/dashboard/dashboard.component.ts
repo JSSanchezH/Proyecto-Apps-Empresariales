19f37ca38ecf21dae793b572bc60edbf41ae8096
JSSanchezH <juansebastiansanchezh@gmail.com>
2025-05-27 01:05:15 -0500

    feat(dashboard): display employee data correctly in the dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -3,58 +3,6 @@ import { TopbarComponent } from '../../shared/topbar/topbar.component';
 import { CardsComponent } from './cards/cards.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { RegisterEmployeeComponent } from './register-employee/register-employee.component';
-import { AuthService } from '../../services/auth.service';
-import { EmployeeService } from '../../services/employee.service';
-
-interface Employee {
-  id: number;
-  firstname: string;
-  lastname: string;
-  email: string;
-  phoneNumber: string;
-  hireDate: string;
-  role: {
-    id: number;
-    name: string;
-  };
-  department: {
-    id: number;
-    name: string;
-    headquarter: {
-      id: number;
-      name: string;
-      address: string;
-      phone: number;
-      city: {
-        id: number;
-        name: string;
-        state: {
-          id: number;
-          name: string;
-          country: {
-            id: number;
-            name: string;
-            continent: {
-              id: number;
-              name: string;
-            };
-          };
-        };
-      };
-      company: {
-        id: number;
-        name: string;
-        nit: number;
-        address: string;
-        email: string;
-        typeIndustry: string;
-        urlLogo: string;
-      };
-    };
-  };
-  urlFoto: string;
-  status: boolean;
-}
 
 @Component({
   selector: 'app-dashboard',
@@ -69,18 +17,7 @@ interface Employee {
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  constructor(
-    private authService: AuthService,
-    private employeeService: EmployeeService
-  ) {}
-
-  employees: Employee[] = [];
-
   ngOnInit(): void {
-    this.employeeService.getCompanyData().subscribe((employees) => {
-      this.employees = employees as Employee[];
-      console.log('Employees:', this.employees);
-    });
     // Toggle menu functionality
     const toggle = document.querySelector('.toggle');
     const container = document.querySelector('.container');
